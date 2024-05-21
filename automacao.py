7665e6f3ee515cba97ff27928689bee2f327611e
Rafael Silva <rafaelps.prog@gmail.com>
2024-05-20 22:16:44 -0300

    configuração para verificar se o processo do quantum expresso terminou antes de rodar um novo loop

--- automacao.py
+++ automacao.py
@@ -1,21 +1,23 @@
 import os
 import shutil
+import psutil
 from time import sleep
 
-path = os.getcwd()
 
+# Setting variables, parameter, final and variation.
+param_c = 18.0
+final_c = 8
+var_c = -2
+quantumexpress_process = ''
+path = os.getcwd()
 input_name = 'pt.scf.in'
 output_name = 'pt.scf.out'
 
+# READING INITIAL INPUT FILE
 with open(f'{path}/{input_name}', 'r') as file:
     text = file.readlines()
 
 # PARAM C IS IN LINE 12
-
-# Defining parameter, final and variation.
-param_c = 18.0
-final_c = 8
-var_c = -2
 
 while param_c > final_c -1:
     print(f'AVISO! Iniciando cálculos para C = {param_c} ...')
@@ -28,22 +30,23 @@ while param_c > final_c -1:
         os.rename(f'TEMP{input_name}', input_name)
     except: 
         os.remove(f'TEMP{input_name}')
+
+    # RUNNING QUANTUM EXPRESS
     os.system(f'~/qe-7.0/bin/pw.x < {input_name} > {output_name}')
-    # VERIFY IF EXISTS THE OUTPUT FILE TO CONTINUE
-    output_exists = False
-    while output_exists == False:
-        try:
-            with open(f'{path}/{output_name}', 'r') as file:
-                output_exists = True
-        except IOError:
+
+    # VERIFYING IF QUANTUM EXPRESS PROCCESS IS RUNNING BEFORE CONTINUE
+    process_exists = True 
+    while process_exists == True:
+        process = [proc.name() for proc in psutil.process_iter()]
+        if quantumexpress_process not in process:
+            process_exists = False
+            os.rename(f'{path}/{output_name}', f'{path}/ptC{param_c}.out')
+        else:
             sleep(60*20)
-    #EXTRACT INFOS OF OUTPUT FILE
-
-
+            
     #REMOVE OUTPUT FILE
     print(f'AVISO! Cálculos para {param_c} finalizados!')
     os.remove(output_name)
-
     param_c += var_c
 
 print('Todas os dados foram gerados e coletados.')
